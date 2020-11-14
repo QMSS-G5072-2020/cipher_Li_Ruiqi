320cb818a93eac22356a87310a2f058beaf67753
Ruiqi Li <rl3167@columbia.edu>
2020-11-14 11:38:41 +0800

    adding test

--- tests/test_cipher_rl3167.py
+++ tests/test_cipher_rl3167.py
@@ -3,3 +3,8 @@ from cipher_rl3167 import cipher_rl3167
 
 def test_version():
     assert __version__ == '0.1.0'
+    
+def test_for_not_alphabet():
+    expected = '!ee'
+    actual = cipher_rl3167.cipher('!dd',1,True)
+    assert actual == expected
